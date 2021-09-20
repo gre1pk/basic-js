9d09559c22d964fb19dd84228b4c5b1d38ec7023
gre1pk <raik1996@gmail.com>
2021-09-20 11:40:46 +0300

    feat: add comlite what season task

--- src/what-season.js
+++ src/what-season.js
@@ -1,17 +1,39 @@
-import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from "../extensions/index.js";
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (!date) {
+    return "Unable to determine the time of year!";
+  }
+  if (Object.keys(date).length !== 0 || !(date instanceof Date)) {
+    throw new Error("Invalid date!");
+  }
+  switch (date.getMonth()) {
+    case 11:
+    case 0:
+    case 1:
+      return "winter";
+    case 8:
+    case 9:
+    case 10:
+      return "fall";
+    case 2:
+    case 3:
+    case 4:
+      return "spring";
+    case 5:
+    case 6:
+    case 7:
+      return "summer";
+  }
 }
